0f1d0d6835094ed730c8eb2f1f7f918d92c5f623
blew <lewisbr2001@gmail.com>
2024-02-20 20:22:43 -0700

    phase3 test page up

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,12 +4,21 @@ import spark.*;
 
 public class Server {
 
+    public static void main(String[] args) {
+        new Server().run(8080);
+    }
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/db", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return null;
+            }
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
