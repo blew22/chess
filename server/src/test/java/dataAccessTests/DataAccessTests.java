ce68f36d7df8fd777fadeb02b728ab7bd1a9464b
blew <lewisbr2001@gmail.com>
2024-03-07 18:48:16 -0700

    phase4 made DataAccessTests

---
+++ server/src/test/java/dataAccessTests/DataAccessTests.java
@@ -0,0 +1,14 @@
+package dataAccessTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DataAccessTests {
+
+
+    @Test
+    public void registerUser(){
+        assertTrue(true);
+    }
+}
