ce8ac219ad6c0faefb0199fb515346ec81c4b225
blew <lewisbr2001@gmail.com>
2024-03-19 20:17:22 -0600

    phase5 draw including pieces on board

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -1,6 +1,8 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -20,14 +22,21 @@ public class BoardPrinter {
 
         StringBuilder chessBoard = new StringBuilder();
         for(int r = 8; r > 0 ; r--){
+            chessBoard.append(edgeBGColor + edgeTextColor);
+            chessBoard.append(EMPTY);
+            chessBoard.append(SET_BG_COLOR_RED);
             for(int c = 1; c <= 8; c++){
                 if(board[r][c] == null){
                     chessBoard.append(EMPTY);
                 } else {
-                    chessBoard.append(board[r][c].toString());
+                    ChessPiece.PieceType type = board[r][c].getPieceType();
+                    ChessGame.TeamColor color = board[r][c].getTeamColor();
+                    chessBoard.append(printPiece(type, color));
                 }
             }
-            chessBoard.append("\n");
+            chessBoard.append(edgeBGColor + edgeTextColor);
+            chessBoard.append(EMPTY);
+            chessBoard.append(advanceLine(out));
         }
 
 
@@ -38,15 +47,54 @@ public class BoardPrinter {
 
     private static void drawChessBoard(PrintStream out, String chessBoard){
         setEdgeFormat(out);
-        out.print("    a  b  c  d  e  f  g  h    ");
-        advanceLine(out);
+        out.print(EMPTY + " A  B  C  D  E  F  G  H " + EMPTY);
+        out.print(advanceLine(out));
+        out.print(chessBoard);
     }
 
     private static void setEdgeFormat(PrintStream out){
         out.print(edgeBGColor + edgeTextColor);
     }
-    private static void advanceLine(PrintStream out){
-        out.print(abyssColor + "\n");
+    private static String advanceLine(PrintStream out){
+        return abyssColor + "\n";
+    }
+
+    private static String printPiece(ChessPiece.PieceType type, ChessGame.TeamColor color){
+        if(color == ChessGame.TeamColor.BLACK){
+            switch(type) {
+                case ROOK:
+                    return BLACK_ROOK;
+                case KNIGHT:
+                    return BLACK_KNIGHT;
+                case BISHOP:
+                    return BLACK_BISHOP;
+                case ChessPiece.PieceType.QUEEN:
+                    return BLACK_QUEEN;
+                case KING:
+                    return BLACK_KING;
+                case PAWN:
+                    return BLACK_PAWN;
+                default:
+                    return " X ";
+            }
+        } else {
+            switch(type){
+                case ROOK:
+                    return WHITE_ROOK;
+                case KNIGHT:
+                    return WHITE_KNIGHT;
+                case BISHOP:
+                    return WHITE_BISHOP;
+                case QUEEN:
+                    return WHITE_QUEEN;
+                case KING:
+                    return WHITE_KING;
+                case PAWN:
+                    return WHITE_PAWN;
+                default:
+                    return " X ";
+            }
+        }
     }
 
 
